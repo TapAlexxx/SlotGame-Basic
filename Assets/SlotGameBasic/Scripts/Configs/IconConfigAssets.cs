36d0268e25fe7d460f9e3d15b795d9289705ec64
Oleksandr <137395172+ollavrynovych@users.noreply.github.com>
2025-11-04 05:29:43 +0200

    auto refresh SO

--- Assets/SlotGameBasic/Scripts/Configs/IconConfigAssets.cs
+++ Assets/SlotGameBasic/Scripts/Configs/IconConfigAssets.cs
@@ -1,26 +1,35 @@
-using System.Collections.Generic;
+using System.IO;
+using AYellowpaper.SerializedCollections;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 
 [CreateAssetMenu(fileName = "IconConfigAsset", menuName = "SlotGame/Configs/IconConfigAsset")]
 public class IconConfigAssets : ScriptableObject, IAutoRefreshable
 {
-    public string iconsFolder = "Icons";
-    public Dictionary<string, Sprite> icons = new();
+    public string targetFolder = "Icons";
+    public SerializedDictionary<string, Sprite> icons = new();
     
 #if UNITY_EDITOR
     public void RefreshData()
     {
         icons.Clear();
-
+        var pathToThis = AssetDatabase.GetAssetPath(this);     
+        var folderPath = Path.GetDirectoryName(pathToThis);    
+        var iconsFolder = folderPath + $"/{targetFolder}"; 
+        
         var guids = AssetDatabase.FindAssets("t:Sprite", new[] { iconsFolder });
-
+        
         foreach (var guid in guids)
         {
             string path = AssetDatabase.GUIDToAssetPath(guid);
+
+            var spriteName = Path.GetFileNameWithoutExtension(path);
             var sprite = AssetDatabase.LoadAssetAtPath<Sprite>(path);
-            icons.Add(guid, sprite);
+            
+            icons.Add(spriteName, sprite);
         }
 
         EditorUtility.SetDirty(this);
